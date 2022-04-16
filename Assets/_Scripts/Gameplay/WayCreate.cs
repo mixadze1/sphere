e5353c712114600042a32a5817b0f7d240f0f846
Mihail A <mixadze1@bk.ru>
2022-04-16 18:45:32 +0300

    [add] start game

---
+++ Assets/_Scripts/Gameplay/WayCreate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayCreate : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> wayPrefab;
+    [SerializeField] private GameObject Finish;
+    [SerializeField] private Transform way;
+    [SerializeField] private Vector3 offSet; 
+    private int count;
+    private void Start()
+    {
+        StartCoroutine(spawnWay());
+    }
+    IEnumerator spawnWay()
+    {
+        for (count = 0; count < wayPrefab.Count; count++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            Instantiate(wayPrefab[count], way.position,Quaternion.identity);                   
+            way.position += offSet; 
+        }
+        Instantiate(Finish, way.position - offSet * 0.5f, Quaternion.identity);
+    }
+  
+}
---
+++ Assets/_Scripts/UI/UIManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UManager : MonoBehaviour
+{
+    [SerializeField] private GameObject start;
+    [SerializeField] private GameObject result;
+    private void Start()
+    {
+        
+    }
+}
