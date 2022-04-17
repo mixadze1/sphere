91a1620081c905683576fdaf45a3da5f9bdce679
Mihail A <mixadze1@bk.ru>
2022-04-17 14:22:06 +0300

    ui

---
+++ Assets/_Scripts/Gameplay/FinishGame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishGame : MonoBehaviour
+{
+    [SerializeField] private GameObject winMenu;
+    [SerializeField] private GameObject loseMenu;
+    [SerializeField] private GameObject ResultMenu;
+
+    public void StartFinish(bool checkMenu)
+    {
+        ResultMenu.SetActive(true);
+        if (checkMenu == true)
+        {
+            winMenu.SetActive(true);
+            loseMenu.SetActive(false);
+        }
+        else
+        {
+            winMenu.SetActive(false);
+            loseMenu.SetActive(true);
+        }
+    }
+}
--- Assets/_Scripts/Gameplay/PlayerController.cs
+++ Assets/_Scripts/Gameplay/PlayerController.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private ResultMenu resultMenu;
     [SerializeField] private Rigidbody rigidbody;
     [SerializeField] private Vector3 startPosition;
     [SerializeField] private Vector3 speedWS;
@@ -16,7 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         rigidbody.position = startPosition;
         isCanMove = false;
-       
+
     }
     private void FixedUpdate()
     {
@@ -37,16 +36,20 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.D))
             {
                 MoveAD(oppositeSide = true);
-            }   
+            }
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                resultMenu.Restart();
+            }
         }
     }
     private void MoveWS(bool oppositeSide)
     {
         if (!oppositeSide)
-        { 
-            rigidbody.velocity += speedWS; 
+        {
+            rigidbody.velocity += speedWS;
         }
-       
+
         else
         {
             rigidbody.velocity -= speedWS;
@@ -69,8 +72,8 @@ public class PlayerController : MonoBehaviour
         rigidbody.isKinematic = true;
         rigidbody.position = startPosition;
         isCanMove = true;
-        rigidbody.velocity = new Vector3(0,0,0);
+        rigidbody.velocity = startPosition;
         rigidbody.isKinematic = false;
         isCanMove = false;
-    } 
+    }
 }
